899b280425fc73c71afadedecd9b7af9c36fa88c
Zachary Darneille <zakdarn@gmail.com>
2020-09-10 20:56:28 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,11 @@
 def unsafe?(speed)
-speed > 60 ? true : false
-unless speed < 40
-  puts false
+if speed < 40 || speed > 60
+  true
+else
+  false
+end
 end
 
-
-
 def not_safe?(speed)
-speed < 60 ? true : false
-speed > 40 ? true : false
+ speed > 60 || speed < 40 ? true : false
 end
