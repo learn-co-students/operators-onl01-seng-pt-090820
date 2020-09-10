22cb97ee7b13803c7f8dfad665fd5a14264d1909
Anthony Leon <acidknights89@gmail.com>
2020-09-10 05:34:45 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,19 @@
-def unsafe?(speed)
 
-end
 
 
 
 def not_safe?(speed)
-	
+  speed < 40 || speed > 60 ? true : false
 end
-	
 
-
+  
+def unsafe?(speed)
+if speed > 60
+  return true
+  elsif speed < 40
+  return true
+  elsif speed < 60 || speed > 40
+  return false
+  
+end
+end
