2404b3d5774446565e3117eb93d63654fae9fe72
HELEN HARMAN <hlharman@bellsouth.net>
2020-09-11 23:27:24 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,6 +1,8 @@
 def unsafe?(speed) 
 speed > 60 ? "unsafe(speed"): "safe"(speed)" 
+end
 speed < 40 ? "unsafe(speed)": "safe(speed)"
+end 
 speed >40 <60 ? "unsafe(speed)": "safe(speed)"
 end 
 end 
