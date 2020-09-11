b8683f2f5ccb14956be3958b84d76d64b8bb5c24
HELEN HARMAN <hlharman@bellsouth.net>
2020-09-11 23:58:24 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,15 +1,11 @@
 def unsafe?(speed) 
-speed >40 or speed < 60 ? "unsafe(speed)": "safe(speed)"
+speed >40 || < 60 ? "unsafe(speed)": "safe(speed)"
 end 
 
 
 
     
-def not_safe?(speed)
-  speed == 60
-speed < 40 ? "true" : "false"
-end 
-end 
+
 
 	
 
